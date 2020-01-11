b7bb71f6b680195ecd487dd620bfcdb067cf5ce5
Amy Franz <amy.franz@gmail.com>
2020-01-11 02:22:21 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,12 @@
-# Code your solution here!
+def run_guessing_game
+  random_num = rand(6) + 1
+  $stdout.puts("please guess a number from 1 - 6")
+  user_input = gets
+  if user_input == random_num.to_s
+    $stdout.puts("You guessed the correct number!")
+  elsif user_input == 'exit'
+    $stdout.puts("Goodbye!")
+  else
+   $stdout.puts("Sorry! The computer guessed #{random_num}.") 
+ end
+end
